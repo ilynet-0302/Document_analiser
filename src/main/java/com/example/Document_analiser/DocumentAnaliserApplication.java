98494e43206f0dbabe8adb5b08ce6de547fd0cca
Iliya <43601053+ilynet-0302@users.noreply.github.com>
2025-07-06 15:32:16 +0300

    first commit

---
+++ src/main/java/com/example/Document_analiser/DocumentAnaliserApplication.java
@@ -0,0 +1,13 @@
+package com.example.Document_analiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAnaliserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentAnaliserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Document_analiser/DocumentAnaliserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Document_analiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAnaliserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
